6dd1b4b4b120cee15facf9a09df33f2335909a4d
David Pacheco <126829964+daviddpacheco@users.noreply.github.com>
2023-04-05 22:39:24 -0300

    Add files via upload

---
+++ login.py
@@ -0,0 +1,11 @@
+senha = input("Digite sua senha")
+usuario = input("Digite seu usuario")
+
+nome = "davidd"
+password = "551462"
+
+while usuario != nome and senha != password:
+    print("errou comédia")
+    senha = input("Digite sua senha: ")
+    usuario = input("Digite seu usuario: ")
+print("acerto")
---
+++ main.py
@@ -0,0 +1,6 @@
+print("Ganhei uma coisinha do igores")
+resposta = input("quer ver?")
+if (resposta != "sim"):
+    print("PEGA A CALCULADORA DO PAI")
+
+
